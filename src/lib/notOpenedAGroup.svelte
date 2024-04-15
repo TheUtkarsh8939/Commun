acec25cbb424ad5ff6a185644cb723955476cd1a
TheUtkarsh8939 <utkarshchandel2012@gmail.com>
2024-04-15 20:32:16 +0530

    Forgot to run add. Whoops!😅

---
+++ src/lib/notOpenedAGroup.svelte
@@ -0,0 +1,49 @@
+<main>
+  <div class="logo">
+    <img src="./logo.png" alt="logo" />
+  </div>
+  <div class="text">Welcome to Sparshak</div>
+  <div class="how-to-join">
+    To Join Click on the Top-Right button on navbar and then click on Join A
+    group, eneter the user ID and you are joined to the group
+  </div>
+</main>
+
+<style lang="scss">
+  @import url("https://fonts.googleapis.com/css2?family=Acme&display=swap");
+  main {
+    height: 100%;
+    width: 100%;
+    display: flex;
+    align-items: center;
+    justify-content: center;
+    flex-direction: column;
+    color: white;
+  }
+  .logo {
+    height: 200px;
+    aspect-ratio: 1/1;
+  }
+  .lines {
+    background-color: white;
+    height: 2px;
+    width: 120px;
+    border-radius: 20px;
+  }
+  #second-line {
+    rotate: 90deg;
+  }
+  .text {
+    margin-top: 40px;
+    font-family: "VIbur";
+    font-size: 2.5rem;
+  }
+  .how-to-join {
+    display: flex;
+    text-align: center;
+    font-family: "Acme";
+    flex-direction: column;
+    font-weight: 400;
+    font-style: normal;
+  }
+</style>
