6bd12a15a5cd3987d3a32a97f44f296e6009dc55
TheUtkarsh8939 <utkarshchandel2012@gmail.com>
2024-05-16 13:53:21 +0530

    BUG FIX: Service Worker not working Properly

--- public/firebase-messaging-sw.js
+++ public/firebase-messaging-sw.js
@@ -1,5 +1,37 @@
+importScripts('https://storage.googleapis.com/workbox-cdn/releases/6.4.1/workbox-sw.js');
 importScripts('https://www.gstatic.com/firebasejs/9.14.0/firebase-app-compat.js')
 importScripts('https://www.gstatic.com/firebasejs/9.14.0/firebase-messaging-compat.js')
+workbox.routing.registerRoute(
+  // Match HTML, CSS, JavaScript, images, and videos
+  ({ url }) => url.pathname.match(/^(.*)\.(html|css|js|png|jpg|gif|svg|mp4|webm|ogg)$/i),
+  new workbox.strategies.NetworkFirst({
+    cacheName: 'network-first-cache',
+  })
+);
+
+// Cache all other resources
+workbox.routing.registerRoute(
+  ({ url }) => !url.pathname.match(/^(.*)\.(html|css|js|png|jpg|gif|svg|mp4|webm|ogg)$/i),
+  new workbox.strategies.CacheOnly({
+    cacheName: 'cache-only-cache',
+  })
+);
+
+// Precache app resources
+workbox.precaching.precacheAndRoute([
+    // List of resources to precache
+    { url: '/', revision: '1' },
+    { url: '/index.html', revision: '1' },
+    { url: '/Ai.png', revision: '1' },
+    { url: '/friendship.jpg', revision: '1' },
+    { url: '/google.svg', revision: '1' },
+    { url: '/load.gif', revision: '1' },
+    { url: '/logo.png', revision: '1' },
+    { url: '/offline.html', revision: '1' },
+    { url: '/pg.css', revision: '1' },
+    { url: '/pg2.css', revision: '1' }
+  
+  ]);
 let config =  {
     apiKey: "AIzaSyDyY5F4FGDsL4al0PagVTFOZzZ0UDIY6Vw",
     authDomain: "friendsforever-9cc00.firebaseapp.com",
@@ -11,3 +43,4 @@ let config =  {
 }
 const app = firebase.initializeApp(config)
 const messaging = firebase.messaging()
+
--- public/sw.js
+++ public/sw.js
@@ -8,21 +8,19 @@ workbox.routing.registerRoute(
   })
 );
 
-// Cache all other resources
-workbox.routing.registerRoute(
-  ({ url }) => !url.pathname.match(/^(.*)\.(html|css|js|png|jpg|gif|svg|mp4|webm|ogg)$/i),
-  new workbox.strategies.CacheOnly({
-    cacheName: 'cache-only-cache',
-  })
-);
 
 // Precache app resources
 workbox.precaching.precacheAndRoute([
   // List of resources to precache
   { url: '/', revision: '1' },
   { url: '/index.html', revision: '1' },
-  { url: '/style.css', revision: '1' },
-  { url: '/app.js', revision: '1' },
-  { url: '/images/logo.png', revision: '1' },
-  { url: '/video.mp4', revision: '1' },
+  { url: '/Ai.png', revision: '1' },
+  { url: '/friendship.jpg', revision: '1' },
+  { url: '/google.svg', revision: '1' },
+  { url: '/load.gif', revision: '1' },
+  { url: '/logo.png', revision: '1' },
+  { url: '/offline.html', revision: '1' },
+  { url: '/pg.css', revision: '1' },
+  { url: '/pg2.css', revision: '1' }
+
 ]);
